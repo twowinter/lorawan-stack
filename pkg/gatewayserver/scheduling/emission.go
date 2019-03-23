e668f1530713161318192fbb7d09281a9fb91629
Johan Stokking <johan@thethingsnetwork.org>
2019-03-23 01:06:57 +0100

    gs: Add emission overlapping method

--- pkg/gatewayserver/scheduling/emission.go
+++ pkg/gatewayserver/scheduling/emission.go
@@ -78,6 +78,15 @@ func (em Emission) AfterWithOffAir(other Emission, toa frequencyplans.TimeOffAir
 	return time.Duration(em.Starts() - other.EndsWithOffAir(toa))
 }
 
+// OverlapsWithOffAir returns whether the given emission overlaps with this emission, considering time-off-air.
+func (em Emission) OverlapsWithOffAir(other Emission, toa frequencyplans.TimeOffAir) bool {
+	emBegins, emEnds := em.Starts(), em.EndsWithOffAir(toa)
+	otherBegins, otherEnds := other.Starts(), other.EndsWithOffAir(toa)
+	return emBegins <= otherBegins && emEnds > otherBegins ||
+		emBegins < otherEnds && emEnds > otherEnds ||
+		otherBegins <= emBegins && otherEnds > emEnds
+}
+
 // Emissions is an list of emissions.
 type Emissions []Emission
 
